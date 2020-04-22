1056fcd645d0954539d1f00e0c7403d24df97913
Jensen Jori <s103466@ap.be>
2020-04-22 19:04:49 +0200

    Login pagina zonder LDAP

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+//import { IonicAuth, IonicAuthOptions } from '@ionic-enterprise/auth';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationService {
+  private router: Router;
+
+  constructor() {}
+
+  
+}
