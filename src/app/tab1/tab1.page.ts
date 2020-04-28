06158a6e96aaa259a178a129ae7449cd4be1926c
Younes Hl <s109442@ap.be>
2020-04-28 16:39:57 +0200

    Sorting/filter verbetering

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,44 +14,87 @@ export class Tab1Page {
   melding: any;
   meldingLijst: any = [];
   kopieLijstVanMeldingen: any = [];
+  actieveLijstVanMeldingen: any = [];
+  sortVal: any;
+  toggle: boolean;
 
-  constructor(private ms: MeldingService, private alertCtrl: AlertController, private navCtrl: NavController, private router: Router, private activatedRoute: ActivatedRoute) {
-    this.melding = this.activatedRoute.snapshot.params['melding'];
+  constructor(private ms: MeldingService, private alertCtrl: AlertController,
+              private navCtrl: NavController, private router: Router, private activatedRoute: ActivatedRoute) {
+    this.melding = this.activatedRoute.snapshot.params.melding;
     this.lijstMeldingen();
-
-  }
-
-  initializeItems(): void {
-    this.meldingLijst = this.kopieLijstVanMeldingen;
-  }
-
-  async searchItems(e) {
-    console.log(this.meldingLijst);
-    this.initializeItems();
-    const val: string = e.target.value;
-
-    if (!val) { return; }
-
-    if (val.trim() !== '') {
-      this.meldingLijst = this.kopieLijstVanMeldingen.filter((item) => {
-        return (item.type.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.melder.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.datum.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.locatie.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.pNummer.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.status.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            //(item.categorie.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.beschrijving.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-            (item.locatiebeschr.toString().toLowerCase().indexOf(val.toLowerCase()) > -1);
-      });
-    }
+    this.sortVal = ' ';
   }
 
   lijstMeldingen() {
     this.ms.getAlleMeldingen().subscribe(data => {
       console.log(data);
       this.meldingLijst = data;
-      this.kopieLijstVanMeldingen = this.meldingLijst;
+      this.activeList();
+    });
+  }
+
+  async searchItems(e) {
+    const val: string = e.target.value;
+
+    this.activeList();
+    if (val.trim() !== '') {
+      this.kopieLijstVanMeldingen = this.actieveLijstVanMeldingen.filter((item) => {
+        return (item.type.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.melder.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.datum.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.locatie.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.pNummer.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.status.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            // (item.categorie.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.beschrijving.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+            (item.locatiebeschr.toString().toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+  }
+
+  async toggleOpdDef() {
+    this.activeList();
+  }
+
+  activeList() {
+    let ch;
+    if (this.toggle) { ch = 'Defect'; } else { ch = 'Opdracht'; }
+    this.actieveLijstVanMeldingen = this.meldingLijst;
+    this.actieveLijstVanMeldingen = this.meldingLijst.filter((item) => {
+      return (item.type.toString().toLowerCase().indexOf(ch.toLowerCase()) > -1) || item.type.toString() === '';
+    });
+    this.kopieLijstVanMeldingen = this.actieveLijstVanMeldingen;
+  }
+
+  async sortAll() {
+    this.kopieLijstVanMeldingen = this.kopieLijstVanMeldingen.sort((n1, n2) => {
+      if (this.sortVal === 'datum') {
+        return n1.datum - n2.datum;
+      } else if (this.sortVal === 'type') {
+        if (n1.type > n2.type) {
+          return 1;
+        }
+        if (n1.type < n2.type) {
+          return -1;
+        }
+        return 0;
+      } else if (this.sortVal === 'locatie') {
+        if (n1.locatie > n2.locatie) {
+          return 1;
+        }
+        if (n1.locatie < n2.locatie) {
+          return -1;
+        }
+        return 0;
+      } else if (this.sortVal === 'status') {
+        if (n1.status > n2.status) {
+          return 1;
+        }
+        if (n1.status < n2.status) {
+          return -1;
+        }
+        return 0;
+      }
     });
   }
 
