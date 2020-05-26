2162c2b1aba7436e1a2431d57dfb26fcfb3e9c5b
Jordan Verlinden <verlinden.jordan@gmail.com>
2020-05-26 16:28:38 +0200

    merge with CreateTask fix

--- src/app/melding/melding.page.spec.ts
+++ src/app/melding/melding.page.spec.ts
@@ -21,4 +21,25 @@ describe('MeldingPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    //check if date picker component starts invisible
+    it('date picker should be hidden', () => {
+      expect(component.showDateSelector).toBeFalsy();
+    });
+  
+    //check if date is current date
+    it('default date should be now', () => {
+      expect(component.date).toEqual(new Date());
+    });
+  
+      //check if date is current date
+      it('default date should be now', () => {
+        expect(component.date).toEqual(new Date());
+      });
+  
+    //check if request date start empty
+    it('request date should be empty', () => {
+      expect(component.requestDate).toBeUndefined();
+    });
+
 });
